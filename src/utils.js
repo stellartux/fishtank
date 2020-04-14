99c846227a4cdf099c7fcbc3cda3bbabf10facc4
Danny Robinson <dannyrobinson@gmx.com>
2020-04-14 14:54:13 +0100

    Shrink utils

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,13 @@
-export const $ = (selector, parent = document) => parent.querySelector(selector)
-export const $$ = (sel, par = document) => Array.from(par.querySelectorAll(sel))
-export const sum = (a = 0, b = 0) => a + b
-export const prod = (a = 1, b = 1) => a * b
-export const clamp = (val, min, max) => Math.max(min, Math.min(max, val))
-export const wrap = (val, min, max) => val % (max - min) + min
-export const range = function*(min, max, step = 1) {
-  for (let i = min; i < max; i += step) yield i
-}
+export const $ = (selector, parent = document) =>
+    parent.querySelector(selector),
+  $$ = (sel, par = document) => Array.from(par.querySelectorAll(sel)),
+  sum = (a = 0, b = 0) => a + b,
+  prod = (a = 1, b = 1) => a * b,
+  clamp = (val, min, max) => Math.max(min, Math.min(max, val)),
+  wrap = (val, min, max) => (val % (max - min)) + min,
+  range = function*(min, max, step = 1) {
+    for (let i = min; i < max; i += step) yield i
+  }
 let debounceRef
 export function debounce(fn, time = 100) {
   if (debounceRef) clearTimeout(debounceRef)
