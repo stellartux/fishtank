a3a9f77636ad5800e4f4df6d73a9928654c94fed
Danny Robinson <dannyrobinson@gmx.com>
2020-04-20 14:50:59 +0100

    Utils - withinRange, isByte

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,17 @@
 export const $ = (selector, parent = document) =>
     parent.querySelector(selector),
   $$ = (sel, par = document) => Array.from(par.querySelectorAll(sel)),
-  sum = (a = 0, b = 0) => a + b,
+  sum = (a = 0, b = 0) => Number(a) + Number(b),
   prod = (a = 1, b = 1) => a * b,
   clamp = (val, min, max) => Math.max(min, Math.min(max, val)),
   wrap = (val, min, max) => (val % (max - min)) + min,
   range = function*(min, max, step = 1) {
     for (let i = min; i <= max; i += step) yield i
   },
-  unique = (val, ind, arr) => ind === arr.indexOf(val)
+  withinRange = (val, min, max, step = 0) =>
+    val >= min && val <= max && (!step || Number.isInteger((val - min) / step)),
+  unique = (val, ind, arr) => ind === arr.indexOf(val),
+  isByte = val => withinRange(val, 0, 255, 1)
 let debounceRef
 export function debounce(fn, time = 100) {
   if (debounceRef) clearTimeout(debounceRef)
--- test/utils_test.js
+++ test/utils_test.js
@@ -2,7 +2,7 @@ import { assertEquals } from '../devdeps.ts'
 import * as Utils from '../src/utils.js'
 
 Deno.test({
-  name: 'clamp',
+  name: 'Utils.clamp()',
   fn: function() {
     assertEquals(Utils.clamp(-1.5, -1, 1), -1)
     assertEquals(Utils.clamp(0.5, -1, 1), 0.5)
@@ -13,24 +13,36 @@ Deno.test({
   },
 })
 Deno.test({
-  name: 'sum',
+  name: 'Utils.sum()',
   fn: function() {
     assertEquals([1, 2, 3, 4].reduce(Utils.sum), 10)
     assertEquals([5, 6, 7, 8, 9].reduce(Utils.sum), 35)
+    assertEquals([1, '1', 1].reduce(Utils.sum), 3)
+    assertEquals([1, undefined, , 1].reduce(Utils.sum), 2)
   },
 })
 Deno.test({
-  name: 'prod',
+  name: 'Utils.prod()',
   fn: function() {
     assertEquals([1, 2, 3, 4].reduce(Utils.prod), 24)
     assertEquals([5, 6, 7, 8, 9].reduce(Utils.prod), 15120)
   },
 })
 Deno.test({
-  name: 'range',
+  name: 'Utils.range()',
   fn: function() {
     assertEquals(Array.from(Utils.range(-1, 1)), [-1, 0, 1])
     assertEquals(Array.from(Utils.range(1, 7, 2)), [1, 3, 5, 7])
+  },
+})
+Deno.test({
+  name: 'Utils.withinRange()',
+  fn: function() {
+    assertEquals(Utils.withinRange(5, 1, 10), true)
+    assertEquals(Utils.withinRange(5, 1, 10, 1), true)
+    assertEquals(Utils.withinRange(5.5, 1, 10), true)
+    assertEquals(Utils.withinRange(5.5, 1, 10, 1), false)
+    assertEquals(Utils.withinRange(1, 2, 3), false)
   },
 })
 
